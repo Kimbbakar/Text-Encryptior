0c4836462e912fcf38daf850e88bdd7499f8e0ce
Kimbbakar <chowdhuryosman04@gmail.com>
2016-05-20 16:31:18 +0600

    Added encryption function

--- main.py
+++ main.py
@@ -16,9 +16,47 @@
 #
 import webapp2
 
+front="""
+	<div>
+	<b>
+	<h1>Welcome to Word encryptor !</h1>
+	</div>
+
+	<form methom="post">
+		<h3>Post your text here </h3>
+		<input type="text" name="text" value="%(text)s">
+
+	</form>
+
+"""
+
+def enprypt(self,s):
+  s=s.lower();
+  s=list(s);
+  for i in range(len(s)):
+    if (s[i].isalpha()==True):
+      s[i]=chr( ( (ord(s[i])-97)+13)%26+97 );
+
+  x=chr(ord(s[0])-32);
+  s[0]=x;
+  s="".join(s)
+  return s
+
+def escape_html(s):
+  s=s.replace("&","&amp;")
+  s=s.replace(">","&gt;")
+  s=s.replace("<","&lt;")
+  s=s.replace('"',"&quot;")
+  return s;
+
+
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
+  def get(self):
+    self.response.out.write(front%{"text":"" } )
+  def post(self):
+  	text=self.requst.get('text')
+  	self.response.out.write(text)
+#    self.response.out.write(front%{"text":(text ) } )
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
